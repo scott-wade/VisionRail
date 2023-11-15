55e418137217bdb04c3a7fc78f617ce09dc2c424
scott-wade <stwade@bu.edu>
2023-11-15 12:23:49 -0500

    Created main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+
+# Open images
+
+# Convert image to grayscale
+
+# Apply mask to image
+
+# Normalize image
+
+# Perform background subtraction
+
+# Draw contour
+
+# Classify objects
+
+# Display images
